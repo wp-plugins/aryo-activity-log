b3e7f9c1d2e3c40dd21f7a3e3e253e95e73cc0f4
KingYes <KingYes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-27 05:59:19 +0000

    Tagging 2.1.1
    git-svn-id: https://plugins.svn.wordpress.org/aryo-activity-log/tags/2.1.1@903338 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-aal-hooks.php
+++ classes/class-aal-hooks.php
@@ -19,6 +19,8 @@ class AAL_Hooks {
 		include( plugin_dir_path( ACTIVITY_LOG__FILE__ ) . '/hooks/class-aal-hook-taxonomy.php' );
 		include( plugin_dir_path( ACTIVITY_LOG__FILE__ ) . '/hooks/class-aal-hook-theme.php' );
 		include( plugin_dir_path( ACTIVITY_LOG__FILE__ ) . '/hooks/class-aal-hook-widgets.php' );
+		include( plugin_dir_path( ACTIVITY_LOG__FILE__ ) . '/hooks/class-aal-hook-core.php' );
+		include( plugin_dir_path( ACTIVITY_LOG__FILE__ ) . '/hooks/class-aal-hook-comments.php' );
 		
 		new AAL_Hook_User();
 		new AAL_Hook_Attachment();
@@ -29,5 +31,7 @@ class AAL_Hooks {
 		new AAL_Hook_Taxonomy();
 		new AAL_Hook_Theme();
 		new AAL_Hook_Widgets();
+		new AAL_Hook_Core();
+		new AAL_Hook_Comments();
 	}
 }
