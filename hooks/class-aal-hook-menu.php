9156dc5b0c512f30705feeae9b7242d4d1910fd5
KingYes <KingYes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-18 16:41:52 +0000

    Tagging 2.1.10
    git-svn-id: https://plugins.svn.wordpress.org/aryo-activity-log/tags/2.1.10@992707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hooks/class-aal-hook-menu.php
+++ hooks/class-aal-hook-menu.php
@@ -3,19 +3,37 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class AAL_Hook_Menu extends AAL_Hook_Base {
 
-	public function hooks_menu_updated( $nav_menu_selected_id ) {
+	public function hooks_menu_created_or_updated( $nav_menu_selected_id ) {
 		if ( $menu_object = wp_get_nav_menu_object( $nav_menu_selected_id ) ) {
-			aal_insert_log( array(
-				'action'      => 'updated',
-				'object_type' => 'Menu',
-				'object_name' => $menu_object->name,
-			) );
+			if ( 'wp_create_nav_menu' === current_action() )
+				$action = 'created';
+			else
+				$action = 'updated';
+			
+			aal_insert_log(
+				array(
+					'action'      => $action,
+					'object_type' => 'Menu',
+					'object_name' => $menu_object->name,
+				)
+			);
 		}
+	}
+
+	public function hooks_menu_deleted( $term, $tt_id, $deleted_term ) {
+		aal_insert_log(
+			array(
+				'action'      => 'deleted',
+				'object_type' => 'Menu',
+				'object_name' => $deleted_term->name,
+			)
+		);
 	}
 	
 	public function __construct() {
-		add_action( 'wp_update_nav_menu', array( &$this, 'hooks_menu_updated' ) );
-
+		add_action( 'wp_update_nav_menu', array( &$this, 'hooks_menu_created_or_updated' ) );
+		add_action( 'wp_create_nav_menu', array( &$this, 'hooks_menu_created_or_updated' ) );
+		add_action( 'delete_nav_menu', array( &$this, 'hooks_menu_deleted' ), 10, 3 );
 		parent::__construct();
 	}
 
